0276c58ac689f8a4c55bd6b19e53be7cc28ca6a6
moosaraza8877 <moosaraza8877@gmail.com>
2020-05-20 01:26:17 +0500

    Ready to Commit

---
+++ ChallengeForm/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ChallengeForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClick.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(416, 218);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(147, 54);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click Here";
+            this.btnClick.UseVisualStyleBackColor = false;
+            this.btnClick.MouseEnter += new System.EventHandler(this.btnClick_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(972, 520);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+    }
+}
+
---
+++ ChallengeForm/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChallengeForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int xCord = random.Next(50, 300);
+            int yCord = random.Next(50, 300);
+            Point point = new Point(xCord, yCord);
+            btnClick.Location = point;
+        }
+    }
+}
